71d2af31a0fb64483af8644b6abaefd05e0a2cb7
commel <commel@gmail.com>
2014-09-30 11:27:37 +0000

    Testing hinzufügt und Gems

--- template.rb
+++ template.rb
@@ -3,3 +3,19 @@
 #
 # an rails template for jruby with sqlite and full testing equipment.
 #
+
+# gems
+gem 'activerecord-jdbcsqlite3-adapter'
+gem 'sass-rails', '~> 4.0.3'
+
+gem_group :development, :test do
+  gem 'rspec-rails', '~> 2.14.1'
+  gem 'fabrication', '~> 2.11.2'
+  gem 'database_cleaner'
+  gem 'capybara'
+  gem 'forgery'
+end
+
+initializer 'disable_jce_restrictions.rb', <<-CODE
+  java.lang.Class.for_name('javax.crypto.JceSecurity').get_declared_field('isRestricted').tap{|f| f.accessible = true; f.set nil, false}
+end
